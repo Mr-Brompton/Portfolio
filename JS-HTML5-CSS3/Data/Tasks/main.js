dfd3f4faa7af6c9e664e45382b328b9095c96020
Mr-Brompton <mr.brompton@mail.com>
2019-07-06 23:58:12 +0100

    Front-end2

---
+++ JS-HTML5-CSS3/Data/Tasks/main.js
@@ -0,0 +1,6 @@
+myName = "Matt";
+myAge = 30;
+myAim = true;
+myBrain = null;
+
+document.write("<p>Hello, my name is " + myName + ". I am " + myAge + " years old. My aim is " + myAim + " though I wish I had a brain, but at the moment I have " + myBrain + "</p>")
