280e19d26b35092dddce0281110801f2a0de4f7b
mr-brompton <mr.brompton@mail.com>
2019-07-25 23:20:13 +0100

    Rename Marching STring

--- JS-HTML5-CSS3/myFirstGame/js/app.js
+++ JS-HTML5-CSS3/myFirstGame/js/app.js
@@ -12,9 +12,9 @@ function setUpGame() {
 	astImage = new Image();
 	astImage.src = "img/Ast.png";
 	rocketImage.src = "img/rocket.png";
-	
+
 	gameCanvas.getContext("2d").drawImage(rocketImage, Math.random() * 100, Math.random() * 100);
-	
+
 	gameCanvas.addEventListener("mousemove", onMouseMovement);
 	setInterval(onTick, 10);
 }
@@ -28,7 +28,7 @@ function onTick() {
 	var gameCanvas = document.getElementById("gameCanvas");
 	var currentAstNumber = 0;
 	var numberOfEnemies = astXPositions.length;
-	
+
 	if (Math.random() < 1/20)
 	{
 		astYPositions.push(0);
@@ -39,25 +39,25 @@ function onTick() {
 		astYPositions[currentAstNumber] = astYPositions[currentAstNumber] + 1;
 		currentAstNumber = currentAstNumber + 1;
 	}
-	
+
 	gameCanvas.width = 400;		//this erases the contents of the canvas
 	gameCanvas.getContext("2d").drawImage(rocketImage, rocketX, rocketY);
-	
+
 	currentAstNumber = 0;
 	while (currentAstNumber < numberOfEnemies) {
 		gameCanvas.getContext("2d").drawImage(astImage, astXPositions[currentAstNumber], astYPositions[currentAstNumber]);
 		currentAstNumber = currentAstNumber + 1;
 	}
-	
+
 	currentAstNumber = 0;
 	while (currentAstNumber < numberOfEnemies) {
 		if ( ( (rocketX < astXPositions[currentAstNumber] && astXPositions[currentAstNumber] < rocketX + 17) || (astXPositions[currentAstNumber] < rocketX && rocketX < astXPositions[currentAstNumber] + 17) ) && ( (rocketY < astYPositions[currentAstNumber] && astYPositions[currentAstNumber] < rocketY + 17) || (astYPositions[currentAstNumber] < rocketY && rocketY < astYPositions[currentAstNumber] + 17) ) ) {
 			if (alert("You were destroyed by an asteroid!" + " you scored = " + score )){}
 			else {window.location.reload(); };
-			
+
 		}
 		currentAstNumber = currentAstNumber + 1;
-	}	
-	
+	}
+
 	score = score + 1;
 }
